a517046123cf94e1f16086959677eb9408ed57bb
廖小白 <Goatman914@gmail.com>
2022-09-05 22:46:16 +0800

    Update 1.0.0

--- InspectorDrawer/Assets/InspectorDrawer/Sample/InspectorSample.cs
+++ InspectorDrawer/Assets/InspectorDrawer/Sample/InspectorSample.cs
@@ -9,73 +9,73 @@ namespace GMToolKit.Inspector
         [SerializeField, HideInInspector, Inspect]
         private Sectionary<TestClass, int> i;
 
-        // [SerializeField, Inspect] private Sectionary<int, int> sectionary;
+        [SerializeField, Inspect("这是一个可以序列化的字典")] private Sectionary<int, int> sectionary;
 
-        // [Inspect("这是一个不可序列化的类")] private TestClass originClass;
+        [Inspect("这是一个不可序列化的类")] private TestClass originClass;
 
-        // [Inspect("这是一个Int")] private int intTest;
-        // [Inspect] private uint uintTest;
-        // [Inspect] private long longTest;
-        // [Inspect] private ulong uongTest;
-        // [Inspect] private string stringTest;
-        // [Inspect] private float floatTest;
-        // [Inspect] private double doubleTest;
+        [Inspect("这是一个Int")] private int intTest;
+        [Inspect] private uint uintTest;
+        [Inspect] private long longTest;
+        [Inspect] private ulong uongTest;
+        [Inspect] private string stringTest;
+        [Inspect] private float floatTest;
+        [Inspect] private double doubleTest;
 
-        // [Inspect] private TestEnum enumTest;
-        // [Inspect] private Texture unityObjectTest;
-        // [Inspect] private Color colorTest;
-        // [Inspect] private AnimationCurve curveTest;
-        // [Inspect] private Vector2 v2Test;
-        // [Inspect] private Vector3 v3Test;
-        // [Inspect] private Vector4 v4Test;
+        [Inspect] private TestEnum enumTest;
+        [Inspect] private Texture unityObjectTest;
+        [Inspect] private Color colorTest;
+        [Inspect] private AnimationCurve curveTest;
+        [Inspect] private Vector2 v2Test;
+        [Inspect] private Vector3 v3Test;
+        [Inspect] private Vector4 v4Test;
 
-        // [Inspect] private TestEnum[][] intArrayTest;
-        // [Inspect] private List<List<TestClass>> intListTest;
+        [Inspect] private TestEnum[][] enumArrayTest;
+        [Inspect] private List<List<TestClass>> doubleListTest;
 
-        // [Inspect] private Dictionary<TestClass, Texture> dictionaryTest;
-        // [Inspect] private Dictionary<int, TestEnum> dictionaryTest1;
-        // [Inspect] private Dictionary<UnityEngine.Vector2, TestClass> dictionaryTest2;
+        [Inspect] private Dictionary<TestClass, Texture> dictionaryTest;
+        [Inspect] private Dictionary<int, TestEnum> dictionaryTest1;
+        [Inspect] private Dictionary<UnityEngine.Vector2, TestClass> dictionaryTest2;
 
-        // [Button("这是一个方法")]
-        // private void B()
-        // {
-        //     Debug.Log("B");
-        // }
-        // [Button]
-        // private void B(int param)
-        // {
-        //     Debug.Log(param);
-        // }
-        // [Button]
-        // private void B(TestEnum param)
-        // {
-        //     Debug.Log(param);
-        // }
-        // [Button]
-        // private void B(TestClass param)
-        // {
-        //     Debug.Log(param);
-        // }
-        // [Button]
-        // private void B(List<TestClass> param)
-        // {
-        //     Debug.Log(param.Count);
-        // }
-        // [Button]
-        // private void B(TestEnum[] param)
-        // {
-        //     Debug.Log(param.Length);
-        // }
-        // [Button]
-        // private void B(Dictionary<TestClass, TestEnum> param)
-        // {
-        //     Debug.Log(param.Count);
-        // }
-        // [Button]
-        // private void B(Dictionary<UnityEngine.Vector2, TestClass> param, List<TestClass> param1)
-        // {
-        //     Debug.Log(param.Count + param1.Count);
-        // }
+        [Button("这是一个方法")]
+        private void B()
+        {
+            Debug.Log("B");
+        }
+        [Button]
+        private void B(int param)
+        {
+            Debug.Log(param);
+        }
+        [Button]
+        private void B(TestEnum param)
+        {
+            Debug.Log(param);
+        }
+        [Button]
+        private void B(TestClass param)
+        {
+            Debug.Log(param);
+        }
+        [Button]
+        private void B(List<TestClass> param)
+        {
+            Debug.Log(param.Count);
+        }
+        [Button]
+        private void B(TestEnum[] param)
+        {
+            Debug.Log(param.Length);
+        }
+        [Button]
+        private void B(Dictionary<TestClass, TestEnum> param)
+        {
+            Debug.Log(param.Count);
+        }
+        [Button]
+        private void B(Dictionary<UnityEngine.Vector2, TestClass> param, List<TestClass> param1)
+        {
+            Debug.Log(param.Count + param1.Count);
+        }
     }
 
     public enum TestEnum
